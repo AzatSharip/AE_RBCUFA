888cab4fc1c7119eb83cd5e620067d80f2e76a3f
Azat Sharip <60136981+AzatSharip@users.noreply.github.com>
2020-03-14 01:27:24 +0500

    update

--- Courses/courses.py
+++ Courses/courses.py
@@ -2,7 +2,7 @@ import requests
 import sys
 from lxml import html
 from bs4 import BeautifulSoup
-#
+
 # url = 'https://www.cbr.ru/'
 # r = requests.get(url)
 # with open('test.html', 'wb') as output_file:
@@ -13,24 +13,49 @@ with open('test.html', 'rb') as output_file:
 
     soup = BeautifulSoup(text, features="lxml")
 
-    d = soup.find('div', {'class': 'w_data_wrap'}).find('i').get('title')
-    # c = soup.find('div', {'class': 'w_data_wrap'}).find('div').text
-    # data_wrap = d.soup.find_all('i', {'class': 'up'})
-    print(d)
-    # print(c)
+    doll = soup.find_all('div', {'class': 'w_data_wrap'})[0]
+    euro = soup.find_all('div', {'class': 'w_data_wrap'})[1]
 
+    # Находим динамику доллара и евро
+    doll_dynamics = doll.find('i').get('title')
+    euro_dynamics = euro.find('i').get('title')
 
-    # for d in d:
+    #Находим стоимость доллара
+    doll.ins.decompose()
+    doll.i.decompose()
+    for i in doll:
+        doll_val = i
+
+    # Находим стоимость евро
+    euro.ins.decompose()
+    euro.i.decompose()
+    for i in euro:
+        euro_value = i
+
+    #Вычленяем дату
+    date = soup.find_all('div', {'class': 'content'})
+    a = date.find('th', {'class': 'title'})
+    # for d in date:
+    #     a = d.find_all('th', {'class': 'title'})
+    # date = date.find('span', {'class': 'nowrap'})
+    print(a)
+
+    # for i in date:
+    #     date = i
+
+    # #Убираем лишний текст из даты
+    # indx = date.index(' ')
+    # date = date[indx:]
     #
-    #     print(d)
-    # items = doll_list.find_all('div', {'class': ['w_data_wrap']})
-    # print(items)
-    # for item in items:
-    #     movie_link = (tag.name, tag.text)
-    #     print(movie_link)
+    #
+    # print('Курсы валют установленные ЦБ РФ на{}'.format(date))
+    # print('Доллар -- Стоимость: {}, динамика: {}'.format(doll_val, doll_dynamics))
+    # print('Евро -- Стоимость: {}, динамика: {}'.format(euro_value, euro_dynamics))
 
-    # tree = html.fromstring(output_file)
-    # tree.xpath
+
+
+
+
 
 
 
@@ -41,3 +66,13 @@ with open('test.html', 'rb') as output_file:
 
 #sys.exit()
 
+
+
+# doll_dynamics = soup.find('div', {'class': 'w_data_wrap'})
+#     doll_dynamics.ins.decompose()
+#     doll_dynamics.i.decompose()
+#     for i in doll_dynamics:
+#         print(i)
+
+
+#doll_dynamics = soup.find('div', {'class': 'w_data_wrap'}).find('i').get('title')
