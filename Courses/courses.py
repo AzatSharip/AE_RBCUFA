29ca51e7747a6490522b92e971076c81ecbba17b
Azat Sharip <60136981+AzatSharip@users.noreply.github.com>
2020-03-13 19:23:41 +0500

    update

--- Courses/courses.py
+++ Courses/courses.py
@@ -2,24 +2,32 @@ import requests
 import sys
 from lxml import html
 from bs4 import BeautifulSoup
-
-url = 'https://www.cbr.ru/'
-r = requests.get(url)
-with open('test.html', 'wb') as output_file:
-    output_file.write(r.text.encode('utf8'))
+#
+# url = 'https://www.cbr.ru/'
+# r = requests.get(url)
+# with open('test.html', 'wb') as output_file:
+#     output_file.write(r.text.encode('utf8'))
 
 with open('test.html', 'rb') as output_file:
-    soup = BeautifulSoup(output_file, features="lxml")
-    doll_list = soup.find('div', {'class': 'content'})
+    text = output_file.read()
 
-    items = doll_list.find_all('div', {'class': ['w_data_wrap']})
+    soup = BeautifulSoup(text, features="lxml")
 
+    d = soup.find('div', {'class': 'w_data_wrap'}).find('i').get('title')
+    # c = soup.find('div', {'class': 'w_data_wrap'}).find('div').text
+    # data_wrap = d.soup.find_all('i', {'class': 'up'})
+    print(d)
+    # print(c)
+
+
+    # for d in d:
+    #
+    #     print(d)
+    # items = doll_list.find_all('div', {'class': ['w_data_wrap']})
+    # print(items)
     # for item in items:
-    movie_link = doll_list.find('i', {'class': 'up'}).find('title').text
-
-
-
-    print(movie_link)
+    #     movie_link = (tag.name, tag.text)
+    #     print(movie_link)
 
     # tree = html.fromstring(output_file)
     # tree.xpath
@@ -31,5 +39,5 @@ with open('test.html', 'rb') as output_file:
 
 
 
-sys.exit()
+#sys.exit()
 
